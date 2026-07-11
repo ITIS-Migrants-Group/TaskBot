dffeba601ac5eb6ef35467ec068d8a7de1ec543f
vexi <vadimmalkov26@gmail.com>
2026-07-11 17:13:59 +0300

    feat(fix): add put controller

--- NotificationService/src/main/java/ru/itis/migrants/notificationservice/controller/NotificationController.java
+++ NotificationService/src/main/java/ru/itis/migrants/notificationservice/controller/NotificationController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.itis.migrants.notificationservice.dto.CreateNotificationRequest;
 import ru.itis.migrants.notificationservice.dto.NotificationResponse;
-import ru.itis.migrants.notificationservice.service.NotificationService;
+import ru.itis.migrants.notificationservice.dto.UpdateNotificationRequest;import ru.itis.migrants.notificationservice.service.NotificationService;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +19,13 @@ public class NotificationController {
     createNotification(@PathVariable("tg-id") Long tgId,
                        @RequestBody CreateNotificationRequest request) {
         NotificationResponse response = service.createNotification(tgId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping
+    public ResponseEntity<NotificationResponse>
+    updateNotification(@RequestBody UpdateNotificationRequest request) {
+        NotificationResponse response = service.updateNotification(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
